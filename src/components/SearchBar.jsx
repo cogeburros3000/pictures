8a7d7405854c3cbe5181f7c78a0f49aa3ed92173
cogeburros3000 <torresreynasantiago@gmail.com>
2025-05-13 13:26:16 -0700

    ola

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react"
-import "./SearchBar.css"
+import { useState } from "react";
+import "./SearchBar.css";
 
-const SearchBar = ({onsubmit}) =>{
-    const [term, setTerm] = useState ("")
+function SearchBar({ onSubmit }) {
+    const [term, setTerm] = useState("");
 
     const handleFormSubmit = (event) => {
-        event.preventDefault()
-        console.log("Necesito decirle al componente papa sobre los datos")
+        event.preventDefault();
+        onSubmit(term);
+    };
 
-    }
-    const handleChange = (event) =>{
-        setTerm(event.target.value)
-    }
-    console.log("term", term)
-    return(
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    };
+
+    return (
         <div className="search-bar">
             <form onSubmit={handleFormSubmit}>
-                <label> Termino de la busqueda : </label>
-                <input onChange={handleChange} value={term} />
+                <label>Término de la búsqueda:</label>
+                <input
+                    type="text"
+                    value={term}
+                    onChange={handleChange}
+                    placeholder="Escribe algo..."
+                />
             </form>
         </div>
-    )
+    );
 }
+
+export default SearchBar;
