d0531f04c41b37eacf78774f41f74ec1e2db520b
cogeburros3000 <torresreynasantiago@gmail.com>
2025-05-16 19:33:10 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react';
-import SearchBar from './components/SearchBar';
-import ImageList from './components/ImageList';
-import searchImages from './api';
+import React, { useState } from 'react';
 
 function App() {
-  const [images, setImages] = useState([]);
+  // 1. Crear un estado llamado miTexto
+  const [miTexto, setMiTexto] = useState('');
 
-  const handleSubmit = async(term) =>{
-    console.log('usted esta buscando con: ', term);
-    const result = await searchImages(term);
-    setImages(result);
+  // 2. Crear función handleText
+  const handleText = (event) => {
+    // 3. Obtener el valor del input con event.target.value
+    // 4. Actualizar el estado con ese valor
+    setMiTexto(event.target.value);
   };
 
   return (
-    <>
-      <h1>Pictures App</h1>
-      <SearchBar onSubmit = {handleSubmit}/>
-      <ImageList images = {images} />
-    </>
+    <div>
+      <div>
+        Capture su texto
+      </div>
+      {/* 5. Conectar el input con el estado usando value y onChange */}
+      <input value={miTexto} onChange={handleText} />
+      <h3>Su Texto</h3>
+      <p>
+        {/* Mostrar el texto de la caja de entrada aquí */}
+        {miTexto}
+      </p>
+    </div>
   );
 }
 
